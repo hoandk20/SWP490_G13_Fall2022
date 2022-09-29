bafee6eec96ef7fb62ec84c551a953679c8eb173
banh Pao <84974790+abooiris111@users.noreply.github.com>
2022-09-29 22:32:43 +0700

    login form

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import Login from './components/login';
+import Register from './components/register';
+
+function App() {
+  return (
+    <Login/>
+  );
+}
+
+export default App;
